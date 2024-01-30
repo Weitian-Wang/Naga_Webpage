e50f4c99c88e40d118d3d64af524a83d83f2e880
Weitian Wang <weitian@Weitians-MacBook-Pro.local>
2024-01-30 14:21:29 -0800

    pipeline fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    //this line will tell the build to create a file with this name
+    // distDir: "build",
+    output: 'export',
+};
 export default nextConfig;
