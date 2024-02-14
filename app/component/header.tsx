c30b463f72100768ccf3214ea772131b0c8a29ea
Weitian Wang <weitian@Weitians-MacBook-Pro.local>
2024-02-14 11:16:34 -0800

    add responsive ui to nav bar

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import styles from './header.module.css'
+import { useState } from 'react';
+
 function Header() {
+  const [navExpand, setNavExpand] = useState(false);
+
   return (
-        <header className={styles.siteHeader}>
+        <header className={`${styles.siteHeader} ${navExpand?'':styles.active}`}>
         <h1 className={styles.logo} onClick={() => {window.location.replace("/");}}>NAGA</h1>
                 {/* Small screen menu button */}
-        <a className={styles.menuLink}>
-          <span className={styles.bar1}></span>
-          <span className={styles.bar2}></span>
-          <span className={styles.bar3}></span>
+        <a className={`${styles.menuLink} ${navExpand?'':styles.active}`} onClick={() => {setNavExpand(!navExpand)}}>
+            <span className={`${styles.bar1} ${navExpand?'':styles.active}`}></span>
+            <span className={`${styles.bar2} ${navExpand?'':styles.active}`}></span>
+            <span className={`${styles.bar3} ${navExpand?'':styles.active}`}></span>
         </a>
-        <div className={styles.navWrapper}>
+        <div className={`${styles.navWrapper} ${navExpand?'':styles.active}`}>
             <a href="/about" className={styles.navLabel}>About</a>
             <a href="/services" className={styles.navLabel}>Services</a>
             <a href="/partners" className={styles.navLabel}>Partners</a>
